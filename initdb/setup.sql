8591bd668e2625b3f0e8c38e4de81a8eec7e6ed0
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2021-07-02 13:53:50 +0900

    pgweb sample

---
+++ initdb/setup.sql
@@ -0,0 +1,13 @@
+create table users (
+  id int,
+  full_name varchar(50),
+  update_at timestamp default current_timestamp
+);
+
+insert into
+  users
+values
+  (1, 'hoge'),
+  (2, 'fuga'),
+  (3, 'foo'),
+  (4, 'bar');
